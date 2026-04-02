6f669b3e783685a92c8b3a71ca1715a418afb929
capoverse <137749446+capoverse@users.noreply.github.com>
2026-04-02 16:08:22 +0800

    Update options.js

--- netlify/functions/options.js
+++ netlify/functions/options.js
@@ -1,13 +1,13 @@
 const yahooFinance = require("yahoo-finance2").default;
 
 exports.handler = async (event) => {
-  const { ticker } = event.queryStringParameters;
+  const { ticker, days } = event.queryStringParameters;
   try {
     const quote = await yahooFinance.quote(ticker);
     const chain = await yahooFinance.options(ticker);
     return {
       statusCode: 200,
-      body: JSON.stringify({ quote, chain })
+      body: JSON.stringify({ quote, chain, days })
     };
   } catch (err) {
     return { statusCode: 500, body: err.toString() };
