cd2715d787edc11e6cc6dfe8c3b1a9c8e161c128
capoverse <137749446+capoverse@users.noreply.github.com>
2026-04-02 15:21:27 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React from "react";
+import OptionCalculator from "./OptionCalculator";
 
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Option Yield Analyzer</h1>
+      <OptionCalculator />
+    </div>
+  );
+}
+
+export default App;
